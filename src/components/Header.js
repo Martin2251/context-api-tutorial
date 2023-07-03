0888519d63b22f0dd5254edbeedf1936a496e06f
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-07-03 22:14:20 +0200

    start of context

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <span> React Context API</span>
+        <ul className='nav'>
+            
+
+        </ul>
+
+
+    </div>
+  )
+}
+
+export default Header
