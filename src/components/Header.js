286e9c56cb5b223ea7cfc532af357a55a899aef0
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-07-05 09:35:17 +0200

    added styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import "./styles.css"
 
 const Header = () => {
   return (
     <div>
         <span> React Context API</span>
         <ul className='nav'>
-            
+            <li className='prod'>
+                <Link to="/">Home Page</Link>
+                <Link to="/">Home Page</Link>
+            </li>
+            <li className='prod1'>
+            <Link to="/">cart</Link>
+            </li>
+
 
         </ul>
 
